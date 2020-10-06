dd4783e281f4de9093a8485e61e7d66fd15cce77
Leobardo Arguelles <leobardomed@gmail.com>
2020-10-05 20:51:48 -0700

    Add templates and first login draft
    Extra templates were added for future pages of the app.
    Templates added:
      - login.html
      - new_menu.html
      - new_recipe.html
      - recipes.html
    
    Also the stylesheet was fixed. Previous to this commit the href
    attribute was set to a wrong value, but now it is correct.
    
    Author: Leobardo Arguelles <leobardomed@gmail.com>

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,34 @@ from app import app
 from flask_login import current_user, login_user
 from app.models import User
 from flask import redirect, flash, url_for, render_template
+from app.forms import LoginForm
 
 @app.route('/')
 def index():
     return render_template('base.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/new_menu')
+def new_menu():
+    return render_template('new_menu.html')
+
+@app.route('/new_recipe')
+def new_recipe():
+    return render_template('new_recipe.html')
+
+@app.route('/recipes')
+def show_recipes():
+    return render_template('recipes.html')
